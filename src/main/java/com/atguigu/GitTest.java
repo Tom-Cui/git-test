aaa8a6be0c1be69db3c04d4bf57f0fc27d20ec2d
Tom-Cui <87958217+Tom-Cui@users.noreply.github.com>
2021-07-27 18:14:08 +0800

    GitHub update

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("push-test");
         System.out.println("pull-test1");
         System.out.println("pull-test2");
+        System.out.println("github更新");
     }
 
 }
